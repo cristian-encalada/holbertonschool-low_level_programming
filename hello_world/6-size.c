b23af8a54eb3bf6a93f43d0498c9e29816a76ae2
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-15 13:47:20 -0800

    Task 6. Fix: Small spelling mistake / a int > an int

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,7 +13,7 @@ double doubleType;
 float floatType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
