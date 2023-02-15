7ebf70e8126f7c9482fa43f4709d5d403419e55d
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-15 10:25:50 -0800

    Task 4. Fix: Adding missing special character and fixing Betty style issues

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+/**
+ *  * main - Prints a string with a scape character "
+ *   Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char String1[] = "Programming is like building a multilingual puzzle";        
-	puts(String1);         
-return 0;
+	char String1[] = "\"Programming is like building a multilingual puzzle";
+	puts (String1);
+	return (0);
 }
