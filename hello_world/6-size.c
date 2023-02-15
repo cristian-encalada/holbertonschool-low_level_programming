56cf3f24d5eb5027163aa00eef74cb6bccc0ac54
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-15 13:41:14 -0800

    Task 6. C program that prints the size of various types on the computer it is compiled and run on.

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+/**
+ * main -Entry point
+ * Program that prints the size of a variable using the function sizeof
+ * Return: Always 0 (Success)
+*/
+#include<stdio.h>
+int main(void)
+{
+char charType;
+int intType;
+long longType;
+double doubleType;
+float floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
