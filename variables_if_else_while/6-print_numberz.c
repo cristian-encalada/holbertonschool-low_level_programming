a76de0ee7d241d4178fb5697216307d848304ef7
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-16 08:44:43 -0800

    Task 5. Program that prints all single digit numbers of base 10 starting from 0, followed by a new line, using putchar function

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main -Entry point
+* Program that prints all single digit numbers of base 10 starting from 0,
+* followed by a new line (using putchar function)
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+/** if condition checks the simbol :, because it's after 9 (ascii code)*/
+	if (num == ':')
+		putchar('\n');
+	return (0);
+}
