9263cf37a0f0b44440dcf856370e6c092ad01298
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-16 09:37:38 -0800

    Task 7. Fix. Fixing logic operator <> in for condition

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 /**
 * main -Entry point
-* Program that prints the alphabet in lowercase in reverse order,
-* followed by a new line using only putchar function
+* Program that prints the alphabet in lowercase, followed by a new line
+* inverse order and using putchar
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char abc = 'z';
+	char xyz;
 
-	for (abc = 'z'; abc <= 'a'; abc--)
-		putchar(abc);
-/** if condition checks the simbol `, because it's after z (ascii code)*/
-	if (abc < 'a')
+	for (xyz = 'z'; xyz >= 'a'; xyz--)
+		putchar(xyz);
+	if (xyz < 'a')
 		putchar('\n');
 	return (0);
 }
