ba50a1bc302e0be37d9d9c70c7b4b0734ea1dd64
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-16 14:22:41 -0800

    Task 0. Program that prints _putchar, followed by a new line.

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+* main -Entry point
+* Program that prints _putchar, followed by a new line.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char print[] = "_putchar\n";
+
+	printf("%s", print);
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+#C - Projects (Functions and nested loops)
+> Repository created to work in C related projects <br>
+## Projects completed
+// TO DO
+To be updated later
