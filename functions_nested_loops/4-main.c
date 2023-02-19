223ed83496b38bae40ac4e3bc48318131e20e04d
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-19 07:39:38 -0800

    Task 8. Function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
-
-	r = _islower('H');
+    int r;
+	r = _isalpha('H');
 	_putchar(r + '0');
-	r = _islower('o');
+	r = _isalpha('o');
 	_putchar(r + '0');
-	r = _islower(108);
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
 	_putchar(r + '0');
 	_putchar('\n');
-	return (0);
+return (0);
 }
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+* jack_bauer - function that prints every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59.
+* Return: 0 at the end if no errors
+*/
+void jack_bauer(void)
+{
+	int total_min = 0;
+	int min_dig1 = 0;
+	int min_dig2;
+	int hour_dig1 = 0;
+	int hour_dig2 = 0;
+
+	while (total_min <= 1439)
+	{
+		for (min_dig2 = 0; min_dig2 <= 9; min_dig2++)
+		{
+		if ((hour_dig2 == 9) && (min_dig1 == 5) && (min_dig2 == 9))
+		{
+		hour_dig1++;
+		_putchar(hour_dig1 + '0');
+		}
+		else
+		{
+		_putchar(hour_dig1 + '0');
+		}
+			if ((min_dig1 == 5) && (min_dig2 == 9))
+			{
+			_putchar(hour_dig2 + '0');
+			hour_dig2++;
+			}
+			else
+			{
+			_putchar(hour_dig2 + '0');
+			}
+			if (hour_dig2 > 9)
+			{
+			hour_dig2 = 0;
+			}
+		_putchar(':');
+			if (min_dig2 < 9)
+			{
+			_putchar(min_dig1 + '0');
+			}
+			else
+			{
+			_putchar(min_dig1 + '0');
+			min_dig1++;
+			}
+			if (min_dig1 > 5)
+			{
+			min_dig1 = 0;
+			}
+		_putchar(min_dig2 + '0');
+		_putchar('\n');
+		total_min++;
+		}
+	}
+}
