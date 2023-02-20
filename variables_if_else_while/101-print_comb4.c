58908f969f6cdef330b48dfc9796e9e248129d4c
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-20 13:33:53 -0800

    Task 11. Fix: Removing trailing whitespace

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,7 @@
 * Program that prints all possible combinations of three digits.
 * Numbers must be separated by ,, followed by a space
 * The three digits must be different
-* 012, 120, 102, 021, 201, 210 are considered the same combination 
+* 012, 120, 102, 021, 201, 210 are considered the same combination
 * of the three digits 0, 1 and 2
 * Print only the smallest combination of three digits
 * Numbers should be printed in ascending order
