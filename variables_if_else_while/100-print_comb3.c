4fbba07c9f6d6702348f197723d4567dce953c4c
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-20 13:29:53 -0800

    Task 10. Fix: Updating for condition to include 0-9 numbers

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,13 @@ int main(void)
 {
 	int dig1, dig2;
 
-	for (dig1 = 0; dig1 < 8; dig1++)
+	for (dig1 = 0; dig1 <= 8; dig1++)
 	{
-		for (dig2 = dig1 + 1; dig2 < 9; dig2++)
+		for (dig2 = dig1 + 1; dig2 <= 9; dig2++)
 		{
 			putchar(dig1 + '0');
 			putchar(dig2 + '0');
-			if (dig1 + dig2 < 15)
+			if (dig1 + dig2 < 17)
 			{
 				putchar(',');
 				putchar(' ');
