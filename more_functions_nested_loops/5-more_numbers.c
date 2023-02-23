267784bec3bca5d5d85fdb47215499c6e49fbaa3
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-23 07:02:13 -0800

    Task 5. Fix: Using _putchar only 3 times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,21 @@
 */
 void more_numbers(void)
 {
-	int num, counter = 0;
+	int num, counter = 0, i = 0;
+	char x[] = "1011121314";
 
 	while (counter < 10)
 	{
-		for (num = 0; num <= 14; num++)
+		for (num = 0; num <= 9; num++)
 		{
-			if (num > 9)
-			{
-				_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');
-			}
-			else
-				_putchar(num + '0');
+			_putchar(num + '0');
+		}
+		for (num = 0; num <= 9; num++)
+		{
+			_putchar(x[i]);
+			i++;
+			if (i > 9)
+			i = 0;
 		}
 		_putchar('\n');
 		counter++;
