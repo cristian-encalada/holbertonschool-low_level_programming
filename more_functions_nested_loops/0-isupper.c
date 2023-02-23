595ff6c2c9edaad966e0cb9ce04c994201198d44
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-23 04:47:41 -0800

    Task 0. Function that checks for uppercase character.

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+# include "main.h"
+/**
+* _islower - checks for lowercase character.
+* @c: Returns 1 if the input is lowercase
+* @c: Returns 0 otherwise.
+* Return: 0 at the end if no errors
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
