7b0b6cd827e01e9ed8f0b837b4a314fdfa7c0f12
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-24 09:18:58 -0800

    Task 10. Fix: Fixing iteration just for positive values

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,8 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if (size <= 0)                                                                                                                                                                         _putchar('\n');
+	else
 	for (i = 0; i < size; i++)
 	{
 		for (j = 1; j < size - i; j++)
