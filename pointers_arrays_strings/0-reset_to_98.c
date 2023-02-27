fd75044710e4b6003b89fcefe7bdd44d6c9590b5
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-02-27 08:12:26 -0800

    Task 0. Fix: Adding description for parameter n

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 * reset_to_98 - function that takes a pointer to an int as parameter
 * and updates the value it points to to 98
 * @*n: pointer variable
+* @n: input variable
 */
 void reset_to_98(int *n)
 {
