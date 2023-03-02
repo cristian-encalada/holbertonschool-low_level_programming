e5089ad41122d20670ca846c5ec4f44c70e1f9c2
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-01 18:04:00 -0800

    Task 3. Improving code, removing redundant iterations

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,9 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, output;
+	int i = 0;
 
-	while ((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0'))
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
 		i++;
-	if ((s1[i] == '\0') && (s2[i] == '\0'))
-		output = 0;
-	else if (s1[i] != s2[i])
-		output = s1[i] - s2[i];
-	else if (s2[i] != s1[i])
-		output = s2[i] - s1[i];
-	return (output);
+	return (s1[i] - s2[i]);
 }
