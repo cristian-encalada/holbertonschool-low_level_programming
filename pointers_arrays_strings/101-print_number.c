2b13cdf1a872d1974e6d9e2324314e62af2302f6
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-05 04:52:07 -0800

    Task 9. Fix. Increasing INT values range for bigger numbers

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -3,6 +3,7 @@
 /**
  * print_number - Prints an integer
  * @n: Integer to be printed
+ * Int range values (4 bytes) -2,147,483,648 to 2,147,483,647
  */
 void print_number(int n)
 {
@@ -12,7 +13,7 @@ void print_number(int n)
 	{
 		_putchar('-');
 	}
-	for (c = 1000000; c > 0; c = c / 10)
+	for (c = 1000000000; c > 0; c = c / 10)
 	{
 		if (n / c)
 		{
