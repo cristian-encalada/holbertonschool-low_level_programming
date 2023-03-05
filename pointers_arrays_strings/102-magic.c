b00fcd01ea47fa4e41b5dd06f314046017bee7b8
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-04 18:55:53 -0800

    Task 10. (Advanced) Fix. Adding just one line achieve printing a[2] = 98, followed by a new line. Not using variable a

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
- *(p + 5) = 98; /**This is undefined behavior, probably p[5] is at memory location of a[2]*/
+ *(p + 5) = 98; /**This is undefined behavior. Probably p[5] is at memory location of a[2]*/
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
