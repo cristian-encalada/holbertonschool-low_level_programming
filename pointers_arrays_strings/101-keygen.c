d4efa87621fee3b4d294417aaa0c059b11bc0403
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-05 14:08:27 -0800

    Task 11. (Advanced) Program that generates random valid passwords for the program 101-crackme.

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * checksum - Error detection mechanism that detects the error
+ *		in the data/message in a data communication
+ * @str: input string
+ * Return: Sum of all ASCII values in the string
+ */
+unsigned int checksum(char *str)
+{
+	unsigned int add = 0;
+
+	while (*str)
+		add += *str++;
+	return (add);
+}
+/**
+ * main - Program that generates random valid passwords
+ * for the program 101-crackme.
+ * Return: 0 if no errors
+ * Check the disassembled.s for more info about 101-crackme
+ */
+int main(void)
+{
+	char str[32]; /**Size is 32 because assembly code is cmp DWORD PTR*/
+	unsigned int add;
+
+	srand(time(0));
+	do {
+	for (int i = 0; i < 32; i++)
+		str[i] = rand() % 94 + 32;
+	str[32] = '\0';
+	add = checksum(str);
+	} while (add != 2772); /** 0AD4h in decimal is 2772 */
+	printf("%s", str);
+	return (0);
+}
