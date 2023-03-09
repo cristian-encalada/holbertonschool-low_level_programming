dd4f6cd67e532f3ea6c05b0d528c57d97a1048cb
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-09 08:01:58 -0800

    Task 0. Program that prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: number of parameters
+ * @argv: prints an array of arguments passed to the cli
+ * Return: 0 if no errors
+*/
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	(void) argc;
+	return (0);
+}
