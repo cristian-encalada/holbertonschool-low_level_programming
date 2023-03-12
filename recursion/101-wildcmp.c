350771d73757e6b4e0af2597190df976d20affac
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-11 18:00:15 -0800

    Task 8. Function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+/**
+ * wildcmp - function that compares two strings
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: 1 if the strings are considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
