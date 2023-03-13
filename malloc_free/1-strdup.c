85d110da1af508317793bd76784f3861fd867243
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-13 10:11:10 -0700

    Task 1. Function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - function that swaps the values of two integers
+ * @s: input variable - char type working as a string
+ * Return: an integer after the iteration.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+/**
+* *_strcpy - function that swaps the values of two integers
+* @src: input string (source to be copied)
+* @dest: output string (destination array)
+* Return: the copied array
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0'; /**add '\0' at the end */
+	return (start);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.,
+ * @str: input string
+ * Return: On success, returns a pointer to the duplicated string.
+ * It returns NULL if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	char *array, *aux_pointer;
+	int len = _strlen(str);
+
+	array = malloc(len * sizeof(str));
+	if (array == NULL)
+		exit(1);
+	_strcpy(array, str);
+	aux_pointer = array;
+	return (aux_pointer);
+}
