4a74d791ffe9cffa20fc902200897c010d1db9b1
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-15 19:07:45 -0700

    Task 1. Function that concatenates two strings.

--- more_malloc_free/0-main.c
+++ more_malloc_free/0-main.c
@@ -1,31 +1,19 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
+void *malloc_checked(unsigned int);
+
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *c;
-    int *i;
-    float *f;
-    double *d;
+	void *p;
 
-    c = malloc_checked(sizeof(char) * 1024);
-    printf("%p\n", (void *)c);
-    i = malloc_checked(sizeof(int) * 402);
-    printf("%p\n", (void *)i);
-    f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
-    d = malloc_checked(INT_MAX);
-    printf("%p\n", (void *)d);
-    free(c);
-    free(i);
-    free(f);
-    free(d);
-    return (0);
+	p = malloc_checked(sizeof(char) * 1024);
+	printf("%p\n", p);
+	return (0);
 }
--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _strlen - function that swaps the values of two integers
+ * @s: input variable - char type working as a string
+ * Return: an integer after the iteration.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count); /** +1 to include the \0 character*/
+}
+/**
 * *_strncat - function that concatenates two strings.
 * @src: first string (source)
 * @dest: second string (destination)
@@ -28,17 +44,27 @@ char *_strncat(char *dest, char *src, int n)
 	return (start);
 }
 /**
- * malloc_checked - allocates memory using malloc,
- * @b: input - # of int bytes to be allocated
+ * string_nconcat - concatenates two strings
+ * @s1: input string 1
+ * @s2: input string 2
+ * @n: number of characters to be concatenated (s2)
  * Return: if malloc fails, status value = 98
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
+	int len1 = 0, len2 = 0;
 
-	ptr = _strncat(s2, s1, n);
-	ptr = malloc();
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	ptr = malloc((len1 + len2 + 1) * sizeof(char));
 	if (ptr == NULL)
-		exit(98);
+		exit(1);
+	_strncat(ptr, s1, n);
+	_strncat(ptr, s2, n);
 	return (ptr);
 }
