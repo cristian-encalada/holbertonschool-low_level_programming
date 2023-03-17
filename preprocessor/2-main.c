45b1c842ee7cca849b2071679da96f59018b3a5a
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-17 05:43:43 -0700

    Task 2. Program that prints the name of the file it was compiled from, followed by a new line.

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line.
+ * Return: 0 if no errors
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
