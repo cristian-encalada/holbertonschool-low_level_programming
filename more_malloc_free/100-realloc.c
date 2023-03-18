699561344742675df488c10fc7bf6807f3b8d35e
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-18 15:24:51 -0700

    Task 4. Fix. Adding condition when new_size=0 and ptr is not NULL

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -18,6 +18,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
 	if (ptr == NULL)
 	{
 		reallocated = malloc(new_size);
