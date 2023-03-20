9255a62350bf78c819d58507216ea4c6c1e02292
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-20 05:53:34 -0700

    Task 0. Defining a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - First custom structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
