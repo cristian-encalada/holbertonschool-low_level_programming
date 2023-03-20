580d9abf86a1d3166787841461c8f7efac0f53b4
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-20 11:11:19 -0700

    Task 5. Function that frees memory allocated by previous dog struc

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,35 +2,14 @@
 #include <stdlib.h>
 
 /**
-* _strlen - finds the length of a string
-* @s: input string
-* Return: length of a string
-*/
-
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		count++;
-	}
-	return (count);
-}
-
-/**
  * free_dog - frees memory allocated by dog structure
- * @d: dog array
+ * @d: previous dog struct used
  * Return: nothing
  */
 
 void free_dog(dog_t *d)
 {
-	int i = 0;
-	int len = _strlen(d);
-
-	for (i = 0; i < len; i++)
-		free(d[i]);
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
