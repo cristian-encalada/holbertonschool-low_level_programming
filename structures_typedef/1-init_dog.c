6ee224b16469694625fe931bf012135e009fa431
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-20 06:18:51 -0700

    Task 1. Function that initialize a variable of type struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * function init_dog - Initialize a variable of type struct dog
+ * @d: structure dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Owner's name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
