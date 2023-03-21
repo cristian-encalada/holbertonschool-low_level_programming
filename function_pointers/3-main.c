3ffc9ed9edfb10d6167c555b4c2bb43f28aa6114
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-21 12:51:08 -0700

    Task 3. Program that performs simple math operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,31 +1,21 @@
-#include <stdio.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
+ * main - Gets input from user, converts to int, and passes to get_op_func
+ * @argc: Number of command line arguments
+ * @argv: Array containing command line arguments
  *
- * Return: 0 if false, something else otherwise.
+ * Return: 0
  */
-int is_98(int elem)
+int main(int argc, char *argv[])
 {
-	return (elem == 98);
-}
+	int num1, num2;
+	char *operator;
+	(void) argc;
 
-/**
- * main - check the code .
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int array[20] = {
-		0, -986, 97, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4,
-		5, 6, 7, 8, 9, 10, 11, 12
-	};
-	int index;
-
-	index = int_index(array, 20, &is_98);
-	printf("%d\n", index);
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+	printf("%d\n", get_op_func(operator)(num1, num2));
 	return (0);
 }
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition of two integers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substraction of two integers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - nultiplication of two integers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - division of two integers
+  * @a: input number 1
+  * @b: input number 2
+  * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - modulo of two integers
+  * @a: input number 1
+  * @b: input number 2
+  * Return: the remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
