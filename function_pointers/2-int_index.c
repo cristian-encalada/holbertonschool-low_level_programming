dbf6bf2ee161652ae4f0d981483f56de0d8faace
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-03-21 09:03:57 -0700

    Task 2. Function that searches for an integer.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to be used
+ * @size: size of the array
+ * @cmp: pointer to the function
+ * Return: index of the first element
+ *	If no element matches, return -1
+ *	If size <= 0, return -1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index = 0, first_index;
+
+	if (array == NULL || cmp == NULL)
+		exit(1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (; size > 0; size--)
+	{
+		cmp(array[index]);
+		if (cmp(array[index]) != 0)
+		{
+			first_index = index;
+			break; /* Stop searching once 1st value found */
+		}
+		index++;
+	}
+	return (first_index);
+}
