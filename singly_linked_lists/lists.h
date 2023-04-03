75c1862241d91bf1160d795d904938bf95c691ff
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-03 12:43:21 -0700

    Task 2. Function that adds a new node at the beginning of a list_t list.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +23,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* _LIST_H_ */
