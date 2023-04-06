3f852137975b63b1ab9265d42d5b9114c0ce5c7b
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-06 13:35:21 -0700

    Task 8 - Adding frees after removing nodes

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -38,11 +38,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (temp->next == NULL)
 	{
 		temp->prev->next = NULL;
+		free(temp);
 		return (1);
 	}
 	/* Delete a node at the index specified */
 	temp->next->prev = temp->prev;
 	temp->prev->next = temp->next;
-
+	free(temp);
 	return (1);
 }
