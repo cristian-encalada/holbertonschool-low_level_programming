52f0c53f1bf06828dd43ba981b59b03d145d3ef5
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-06 08:31:20 -0700

    Task 7. Function that inserts a new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,36 +1,26 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
- * @index: index of the node, starting from 0
- * @head: pointer to dlistint_t list.
- * Return: the nth node
- */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	unsigned int count = 0;
-
-	while (head != 0)
-	{
-		if (count == index)
-			break;
-		count++;
-		head = head->next;
-	}
-	if (count != index)
-		return (NULL);
-	return (head);
-}
-/**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @idx: index of the list where the new node should be added. Index starts at 0
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @idx: index of the list where the new node should be added.
  * @h: pointer to dlistint_t list.
- * Return: address of the new node, or NULL if it failed
+ * @n: integer input.
+ * Return: address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp, *node;
+	unsigned int i;
+	dlistint_t *new, *temp, *verify;
 
-	node = get_dnodeint_at_index(*h, idx);
+	verify = *h;
+
+	/* Verify if the index is valid */
+	for (i = 0; i < idx; i++)
+	{
+		if (verify == NULL) /* last node exceeded */
+			return (NULL);
+		verify = verify->next;
+	}
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -42,18 +32,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (*h);
 	}
-	/* Adding the new node at the end of the list */
+	/* Adding the new node at the index specified */
 	temp = *h;
-	while (temp->next != node)
+	while (temp->next != verify)
 	{
 		temp = temp->next;
 	}
+	new->prev = temp;
+	new->next = verify;
 	temp->next = new;
-	new->prev = (new + 1)->prev;
-	new->next = (new - 1)->next;
-	(new + 1)->prev = temp;
-	(new - 1)->next = temp;
-/*	new->prev = temp; */
+	verify->prev = new;
 
 	return (new);
 }
