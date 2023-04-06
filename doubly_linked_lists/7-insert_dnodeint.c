8fa8b1cd70b6198526f7502c0c4de9d889800b0d
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-06 12:36:55 -0700

    Task 7. Modifying update order to handle inverse traversing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = temp;
 	new->next = temp->next;
 	/* updating existing nodes */
-	temp->next = new;
 	temp->next->prev = new;
+	temp->next = new;
 	return (new);
 }
