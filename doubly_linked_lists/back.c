533b7da65c8738ea217ff4e733ceea0019d98a08
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-06 11:51:43 -0700

    Task 7. Changing approach, using previous functions to add at the beginning and at the end of the list

--- doubly_linked_lists/back.c
+++ doubly_linked_lists/back.c
@@ -10,26 +10,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *temp = *h;
 
-	/* Inserts the new node at the beginning of the list */
+	/* inserting the new node at the beginning of the list */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	/* Verify if the index is valid */
-	for (; idx > 1; idx--)
+	for (; idx != 1; idx--)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 	}
-	/* Inserts the new node at the end of the list */
+
+	/* inserting the new node at the end of the list */
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
-	/* Adding the new node at the index specified */
+
+	/* inserting the new node at the index specified */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
+	temp->next->prev = new;
 	temp->next = new;
+
 	return (new);
 }
