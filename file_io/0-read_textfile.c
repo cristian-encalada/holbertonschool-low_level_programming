015a515ca61c1f1c9e7646532b60411a6db1ed49
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-11 09:09:49 -0700

    Task 0. Adding conditions to handle negative cases

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -25,6 +25,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
+	if (o == -1 || r == -1 || w == -1 || (ssize_t)w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
 	free(buffer);
 	close(o);
 
