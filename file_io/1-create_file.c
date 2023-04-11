98f4d342d45843dac1e3219e0dde4984f8a9feb1
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-11 12:21:36 -0700

    Task 1. Function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int _strlen(const char *s);
 /**
  * create_file - function that creates a file.
  * @filename: name of the file to create (output)
@@ -12,28 +13,35 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd;			/* file descriptor */
 
-	if (filename == NULL)
+	if (!filename)		/* filename == NULL */
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (!text_content)	/* text_content == NULL */
+		return (1);
+
+	write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * _strlen - Gets the length of a string
+ * @s: String being evaluated
+ * Return: Length of string or 0 if it fails
+ */
+int _strlen(const char *s)
+{
+	size_t i;
+
+	if (s == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * size_content);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || (ssize_t)w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
-	return (w);
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
 }
