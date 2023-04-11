38437496c58e1a3f5ec50a401f9721578cd03014
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-11 13:03:23 -0700

    Task 1. Simplifying logic to handle negative cases

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,22 +13,22 @@ int _strlen(const char *s);
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;			/* file descriptor */
+	int o, w, len;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	if (text_content != NULL)
+		len = _strlen(text_content);
+
+	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
+	close(o);
 
-	if (write(fd, text_content, _strlen(text_content)) == -1)
-		return (-1);
-
-	close(fd);
 	return (1);
 }
 
