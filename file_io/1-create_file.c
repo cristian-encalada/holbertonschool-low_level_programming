a6de47bbe9ff44254a377effc7dd84df889a8e4b
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-04-11 13:22:24 -0700

    Task 1. Initializing to 0 strlen to avoid junk data in memory

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int _strlen(const char *s);
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
  */
 int _strlen(const char *s)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (s == NULL)
 		return (0);
