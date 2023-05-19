ae1599c1ac52af9879f0f1db6b137d2fd83f52d8
Cristian Encalada Camargo <cris.encalada.camargo@gmail.com>
2023-05-19 16:13:48 -0300

    Task 1. Search for a value in a sorted array of integers using the Binary search algorithm

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,29 +2,36 @@
 
 void print_current_value(size_t size, size_t index);
 /**
- * linear_search - Search for a value in an array of integers
- *                  using the Linear search algorithm
+ * binary_search - Search for a value in an array of integers
+ *                  using the Binary search algorithm
  * @array: A pointer to the first element of the array to search in
  * @size: Number of elements in array
  * @value: Value to search for
- *
+ *          Assume that array will be sorted in ascending order
+ *          Assume that value won’t appear more than once in array
  * Return: The first index where value is located
  *          If value is not present in array or if array is NULL, return -1
  */
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int left = 0, mid;
+	int right = size - 1;
 
 	if (array == NULL)
 		return (-1);
 
-	/* Go through the array sequencially */
-	for (i = 0; i < size; i++)
+	while (left <= right)
 	{
-		print_current_value(i, array[i]);
-		if (array[i] == value)
-			return (i);
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return (mid);			/* Value found, return the index */
+		else if (array[mid] < value)
+			left = mid + 1;			/* Value is in the right half */
+		else
+			right = mid - 1;		/* Value is in the left half */
 	}
+
 	return (-1);	/* Value not found */
 }
 
